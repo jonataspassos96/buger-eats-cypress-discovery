2f12a5287130183540563863d3b44d7a617236c9
Jonatas Passos <jpc080496@gmail.com>
2022-07-28 17:48:13 -0300

    refactor: consume "massa de teste" delivery

--- cypress/integration/registration.spec.js
+++ cypress/integration/registration.spec.js
@@ -2,57 +2,26 @@ import signup from '../pages/SignupPage'
 
 describe('Cadastro', () => {
 
-  it('Usuário deve se tornar um entregador', () => {
+  beforeEach(function () {
+    cy.fixture('deliver').then(d => {
+      this.deliver = d
+    })
+  })
 
-    const deliver = {
-      name: 'Jonatas Passos',
-      cpf: '00000014141',
-      email: 'jonatas@mail.com',
-      whatsapp: '11999999999',
-      address: {
-        postalcode: '04534011',
-        street: 'Rua Joaquim Floriano',
-        number: '1000',
-        details: 'Ap 102',
-        district: 'Itaim Bibi',
-        city_state: 'São Paulo/SP'
-      },
-      delivery_method: 'Moto',
-      cnh: 'cnh-digital.jpg'
-    }
-
+  it('Usuário deve se tornar um entregador', function () {
     signup.go()
-    signup.fillForm(deliver);
-    signup.submit();
+    signup.fillForm(this.deliver.signup)
+    signup.submit()
 
     const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
     signup.modalContentShouldBe(expectedMessage)
-
   })
 
-  it('CPF incorreto', () => {
-
-    const deliver = {
-      name: 'Jonatas Passos',
-      cpf: '000000141AA',
-      email: 'jonatas@mail.com',
-      whatsapp: '11999999999',
-      address: {
-        postalcode: '04534011',
-        street: 'Rua Joaquim Floriano',
-        number: '1000',
-        details: 'Ap 102',
-        district: 'Itaim Bibi',
-        city_state: 'São Paulo/SP'
-      },
-      delivery_method: 'Moto',
-      cnh: 'cnh-digital.jpg'
-    }
-
+  it('CPF incorreto', function () {
     signup.go()
-    signup.fillForm(deliver)
+    signup.fillForm(this.deliver.cpf_invalid)
     signup.submit()
     signup.alertMessageShouldBe('Oops! CPF inválido')
-
   })
+
 })
